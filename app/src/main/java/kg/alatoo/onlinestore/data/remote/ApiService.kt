94a3873f990a832ae867e085e9a582dc50406bab
ysebo <asangulovtimur6@gmail.com>
2025-11-01 21:44:39 +0600

    Add: implemented new routes for products and carts according to https://dummyjson.com/docs/products https://dummyjson.com/docs/carts

--- app/src/main/java/kg/alatoo/onlinestore/data/remote/ApiService.kt
+++ app/src/main/java/kg/alatoo/onlinestore/data/remote/ApiService.kt
@@ -1,11 +1,39 @@
 package kg.alatoo.onlinestore.data.remote
 
+import kg.alatoo.onlinestore.data.remote.dto.CartRequest
+import kg.alatoo.onlinestore.data.remote.dto.CartResponse
+import kg.alatoo.onlinestore.data.remote.dto.CartsListResponse
 import kg.alatoo.onlinestore.data.remote.dto.LoginRequest
 import kg.alatoo.onlinestore.data.remote.dto.LoginResponse
+import kg.alatoo.onlinestore.data.remote.dto.Product
+import kg.alatoo.onlinestore.data.remote.dto.ProductsResponse
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface ApiService {
     @POST("auth/login")
     suspend fun login(@Body request: LoginRequest): LoginResponse
+    @GET("products")
+    suspend fun getProducts(@Query("limit") limit: Int = 20, @Query("skip") skip: Int = 0): ProductsResponse
+    @GET("products/search")
+    suspend fun searchProducts(@Query("q") query: String): ProductsResponse
+
+    @GET("products/{id}")
+    suspend fun getProductById(@Path("id") id: Int): Product
+
+    @GET("products/categories")
+    suspend fun getCategories(): List<String>
+
+    @GET("products/category/{category}")
+    suspend fun getProductsByCategory(@Path("category") category: String): ProductsResponse
+
+    @POST("carts/add")
+    suspend fun addCart(@Body request: CartRequest): CartResponse
+
+    @GET("carts/user/{userId}")
+    suspend fun getUserCarts(@Path("userId") userId: Int): CartsListResponse
 }
+
