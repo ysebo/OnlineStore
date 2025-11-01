e904a3452fa5e0fefdb3d4cdf704c42f78c5bff6
ysebo <asangulovtimur6@gmail.com>
2025-11-02 00:09:16 +0600

    Changed logic for MainActivity theme to use NavGraph.kt

--- app/src/main/java/kg/alatoo/onlinestore/MainActivity.kt
+++ app/src/main/java/kg/alatoo/onlinestore/MainActivity.kt
@@ -3,45 +3,26 @@ package kg.alatoo.onlinestore
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import kg.alatoo.onlinestore.presentation.navigation.NavGraph
 import kg.alatoo.onlinestore.ui.theme.OnlineStoreTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             OnlineStoreTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    NavGraph()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    OnlineStoreTheme {
-        Greeting("Android")
-    }
-}
