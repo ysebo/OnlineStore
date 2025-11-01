a88a016965efdd0b923f7774f275bd3902b39500
ysebo <asangulovtimur6@gmail.com>
2025-11-02 00:05:44 +0600

    Added Theme and color for our app

--- app/src/main/java/kg/alatoo/onlinestore/ui/theme/Color.kt
+++ app/src/main/java/kg/alatoo/onlinestore/ui/theme/Color.kt
@@ -2,10 +2,76 @@ package kg.alatoo.onlinestore.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Purple = Color(0xFF7C3AED)
+val PurpleLight = Color(0xFFA78BFA)
+val PurpleDark = Color(0xFF5B21B6)
+val PurpleContainer = Color(0xFFF3E8FF)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val White = Color(0xFFFFFFFF)
+val OffWhite = Color(0xFFFAFAFA)
+val LightGray = Color(0xFFF5F5F5)
+val Gray = Color(0xFF9CA3AF)
+val DarkGray = Color(0xFF4B5563)
+val Black = Color(0xFF111827)
+
+val Success = Color(0xFF10B981)
+val Error = Color(0xFFEF4444)
+val ErrorLight = Color(0xFFFEE2E2)
+
+val md_theme_light_primary = Purple
+val md_theme_light_onPrimary = White
+val md_theme_light_primaryContainer = PurpleContainer
+val md_theme_light_onPrimaryContainer = PurpleDark
+val md_theme_light_secondary = PurpleLight
+val md_theme_light_onSecondary = White
+val md_theme_light_secondaryContainer = Color(0xFFF3E8FF)
+val md_theme_light_onSecondaryContainer = PurpleDark
+val md_theme_light_tertiary = Color(0xFFEC4899)
+val md_theme_light_onTertiary = White
+val md_theme_light_tertiaryContainer = Color(0xFFFCE7F3)
+val md_theme_light_onTertiaryContainer = Color(0xFF9F1239)
+val md_theme_light_error = Error
+val md_theme_light_onError = White
+val md_theme_light_errorContainer = ErrorLight
+val md_theme_light_onErrorContainer = Color(0xFF7F1D1D)
+val md_theme_light_background = White
+val md_theme_light_onBackground = Black
+val md_theme_light_surface = White
+val md_theme_light_onSurface = Black
+val md_theme_light_surfaceVariant = LightGray
+val md_theme_light_onSurfaceVariant = DarkGray
+val md_theme_light_outline = Gray
+val md_theme_light_outlineVariant = Color(0xFFE5E7EB)
+val md_theme_light_scrim = Color(0xFF000000)
+val md_theme_light_inverseSurface = Color(0xFF1F2937)
+val md_theme_light_inverseOnSurface = OffWhite
+val md_theme_light_inversePrimary = PurpleLight
+
+val md_theme_dark_primary = PurpleLight
+val md_theme_dark_onPrimary = PurpleDark
+val md_theme_dark_primaryContainer = Color(0xFF6D28D9)
+val md_theme_dark_onPrimaryContainer = PurpleContainer
+val md_theme_dark_secondary = Color(0xFFC4B5FD)
+val md_theme_dark_onSecondary = Color(0xFF4C1D95)
+val md_theme_dark_secondaryContainer = Color(0xFF5B21B6)
+val md_theme_dark_onSecondaryContainer = Color(0xFFEDE9FE)
+val md_theme_dark_tertiary = Color(0xFFF9A8D4)
+val md_theme_dark_onTertiary = Color(0xFF831843)
+val md_theme_dark_tertiaryContainer = Color(0xFF9F1239)
+val md_theme_dark_onTertiaryContainer = Color(0xFFFCE7F3)
+val md_theme_dark_error = Color(0xFFFCA5A5)
+val md_theme_dark_onError = Color(0xFF7F1D1D)
+val md_theme_dark_errorContainer = Color(0xFF991B1B)
+val md_theme_dark_onErrorContainer = ErrorLight
+val md_theme_dark_background = Color(0xFF111827)
+val md_theme_dark_onBackground = OffWhite
+val md_theme_dark_surface = Color(0xFF1F2937)
+val md_theme_dark_onSurface = OffWhite
+val md_theme_dark_surfaceVariant = Color(0xFF374151)
+val md_theme_dark_onSurfaceVariant = Color(0xFFD1D5DB)
+val md_theme_dark_outline = Color(0xFF6B7280)
+val md_theme_dark_outlineVariant = Color(0xFF4B5563)
+val md_theme_dark_scrim = Color(0xFF000000)
+val md_theme_dark_inverseSurface = OffWhite
+val md_theme_dark_inverseOnSurface = Color(0xFF1F2937)
+val md_theme_dark_inversePrimary = Purple
